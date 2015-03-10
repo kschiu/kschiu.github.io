ddd297964889c30abef156a184dac119886d1d22
Kevin Chiu <kschiu@andrew.cmu.edu>
2015-03-10 19:46:23 -0400

    Added smooth scroll.
    Retagged ids

---
+++ scroll.js
@@ -0,0 +1,18 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 5000);
+          return false;
+        }
+      }
+    });
+  });
+</script>
