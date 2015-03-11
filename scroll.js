c965d14db4d917ecdcd24b08a8e602df0c2b466f
Kevin Chiu <kschiu@andrew.cmu.edu>
2015-03-10 21:46:12 -0400

    Moved JS to external sheet. Sped up scroll

--- scroll.js
+++ scroll.js
@@ -1,19 +1,16 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script>
-  $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 5000);
-          return false;
-        }
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
       }
-    });
+    }
   });
-</script>
+});
 // Code from https://css-tricks.com/snippets/jquery/smooth-scrolling/
